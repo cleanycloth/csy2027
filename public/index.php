4999cf91f8e98797756efb5c2ef738d9819e7bf7
Aidan Rayner <aidan.rayner@gmail.com>
2020-02-19 12:48:07 +0000

    Updating files, added task list, etc.

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
+        <link rel="stylesheet" type="text/css" href="styles/fonts.css">
+        <!-- Font Awesome Icons - https://fontawesome.com/ -->
+        <script src="https://kit.fontawesome.com/04ac565eb5.js" crossorigin="anonymous"></script>
+        <title>NN Games - Page Name</title>
+    </head>
+
+    <body>
+        <!-- Header -->
+        <header>
+            <a href="#"><h1>NN Games</h1></a>
+        </header>
+
+        <!-- Navigation Bar -->
+        <nav>
+            <ul>
+                <li><a href="#">PlayStation</a></li>
+                <li><a href="#">Xbox</a></li>
+                <li><a href="#">Nintendo</a></li>
+                <li><a href="#">PC</a></li>
+                <li><a href="#">Pre-Owned</a></li>
+                <li><a href="#">Accessories</a></li>
+            </ul>
+        </nav>
+
+        <!-- Main -->
+        <main>
+            <!-- Content Goes Here -->
+        </main>
+
+        <!-- Footer -->
+        <footer>
+            &copy; 2020 NN Games - All Rights Reserved...whatever that means.
+        </footer>
+    </body>
+</html>
