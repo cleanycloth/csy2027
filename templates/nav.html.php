0fa6f10a84287d8381f9346398634a931c1825ac
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-05 18:45:26 +0100

    Updated main navigation bar to show child categories in dropdowns.

--- templates/nav.html.php
+++ templates/nav.html.php
@@ -1,16 +1,16 @@
 <nav class="site-nav">
     <ul>
         <?php foreach($categories as $category): ?>
-            <li><a href="/category?id=<?=$category->category_id;?>"><?=$category->name;?></a></li>
+            <?php if (empty($category->parent_id)):?>
+                <li class="dropdown">
+                <a href="/category?id=<?=$category->category_id;?>"><?=$category->name;?></a>
+                <ul>
+                    <?php foreach($category->getChildCategories() as $childCategory): ?>
+                        <li><a href="/category?id=<?=$childCategory->category_id;?>"><?=$childCategory->name;?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </li>
+            <?php endif;?>
         <?php endforeach; ?>
-        <li class="dropdown">
-            <!-- <i class="fas fa-chevron-up"></i> -->
-            <a href="#">Dropdown Menu</a>
-            <ul>
-                <li><a href="#">Item 1</a></li>
-                <li><a href="#">Item 2</a></li>
-                <li><a href="#">Item 3</a></li>
-            </ul>
-        </li>
     </ul>
 </nav>
