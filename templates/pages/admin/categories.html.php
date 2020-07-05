4ff2c1326c1e323c91b9499f03979bef9a5f936e
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-05 19:11:51 +0100

    Replaced the 'Delete Category' anchor tag in categories.html.php with a form identical in appearance.

--- templates/pages/admin/categories.html.php
+++ templates/pages/admin/categories.html.php
@@ -10,7 +10,10 @@
                         <p class="category-name"><a href="../category.php?category=<?=urlencode(strip_tags($parentCategory->name));?>"><?=htmlspecialchars(strip_tags($parentCategory->name), ENT_QUOTES, 'UTF-8');?></a>
                         <p>Total Products: <?=$parentCategory->getProductsCount();?></p>
                         <p><a href="/admin/categories/edit?id=<?=$parentCategory->category_id;?>"><i class="fas fa-edit"></i> Edit Category</a></p>
-                        <p><a href="/admin/categories/delete?id=<?=$parentCategory->category_id;?>"><i class="fas fa-trash-alt"></i> Delete Category</a></p>
+                        <form action="" method="post">
+                            <input type="hidden" name="category[category_id]" value="<?=$parentCategory->category_id?>">
+                            <button><i class="fas fa-trash-alt"></i> Delete Category</button>
+                        </form>  
                     </article>
                     <?php if (count($parentCategory->getChildCategories()) > 0): //Check if the amount of rows in $childCategories is greater than 0. Used to determine if a parent category has any child categories. ?>
                         <ul class="child-categories">
@@ -20,7 +23,10 @@
                                     <p class="category-name"><a href="../category.php?category=<?=urlencode(strip_tags($childCategory->name));?>"><?=htmlspecialchars(strip_tags($childCategory->name), ENT_QUOTES, 'UTF-8');?></a>
                                     <p>Total Products: <?=$childCategory->getProductsCount();?></p>
                                     <p><a href="/admin/categories/edit?id=<?=$childCategory->category_id;?>"><i class="fas fa-edit"></i> Edit Category</a></p>
-                                    <p><a href="/admin/categories/delete?id=<?=$childCategory->category_id;?>"><i class="fas fa-trash-alt"></i> Delete Category</a></p>
+                                    <form action="" method="post">
+                                        <input type="hidden" name="category[category_id]" value="<?=$childCategory->category_id?>">
+                                        <button><i class="fas fa-trash-alt"></i> Delete Category</button>
+                                    </form>                                
                                 </article>
                             </li>
                             <?php endforeach; ?>
