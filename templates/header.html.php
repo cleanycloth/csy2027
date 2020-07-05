c64e72699f6ece0b8cff01eebe530c409d8d4b3e
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-05 01:25:17 +0100

    Minor update to user panel in header

--- templates/header.html.php
+++ templates/header.html.php
@@ -13,7 +13,7 @@
     <?php if (!isset($_SESSION['isLoggedIn'])): ?>
         <!-- Signed Out -->
         <ul>
-            <li><a href="/login">Sign in <i class="fas fa-sign-in-alt"></i></a></li> |
+            <li><a href="/login">Sign In <i class="fas fa-sign-in-alt"></i></a></li> |
             <li><a href="/register">Register <i class="fas fa-user-plus"></i></a></li>
         </ul>
     <?php else: ?>
@@ -24,7 +24,7 @@
             <?php if (isset($_SESSION['isAdmin'])): ?>
                 <li><a href="/admin">Admin Panel <i class="fas fa-cog"></i></a></li> |
             <?php endif; ?>
-            <li><a href="/logout">Sign out <i class="fas fa-sign-out-alt"></i></a></li>
+            <li><a href="/logout">Sign Out <i class="fas fa-sign-out-alt"></i></a></li>
         </ul>
     <?php endif; ?>
 </div>
