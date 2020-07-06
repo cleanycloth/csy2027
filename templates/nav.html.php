8875a5e0bb21c7a94d17510f5492ee87008520bb
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-06 23:58:50 +0100

    Include name of category in category URL instead of ID.

--- templates/nav.html.php
+++ templates/nav.html.php
@@ -3,10 +3,10 @@
         <?php foreach($categories as $category): ?>
             <?php if (empty($category->parent_id)):?>
                 <li class="dropdown">
-                <a href="/category?id=<?=$category->category_id;?>"><?=$category->name;?></a>
+                <a href="/category?name=<?=urlencode($category->name);?>"><?=$category->name;?></a>
                 <ul>
                     <?php foreach($category->getChildCategories() as $childCategory): ?>
-                        <li><a href="/category?id=<?=$childCategory->category_id;?>"><?=$childCategory->name;?></a></li>
+                        <li><a href="/category?name=<?=urlencode($childCategory->name);?>"><?=$childCategory->name;?></a></li>
                     <?php endforeach; ?>
                 </ul>
             </li>
