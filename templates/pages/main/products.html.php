aa130b6750c5e8ebda7de3b078239ef875553826
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-07 21:36:35 +0100

    Added price below product listngs

--- templates/pages/main/products.html.php
+++ templates/pages/main/products.html.php
@@ -32,7 +32,7 @@
     <div class="ResultSide">
         <div class="Title">
         <h1><?=$pageName;?></h1>
-        <?php if (!empty($products) && count($products) > 0): ?>
+        <?php if (!empty($products)): ?>
             <p>Showing <?=$lowerAmount;?>-<?=$upperAmount;?> of <?=$totalProducts;?> product(s)</p>
             </div>
 
@@ -41,7 +41,8 @@
                     <div class="product">
                         <a href="/product?id=<?=$product->product_id;?>">
                             <img src="<?=$product->image;?>" alt="<?=($product->image != '/images/image-placeholder.jpg') ? htmlspecialchars(strip_tags($product->name), ENT_QUOTES, 'UTF-8') : 'Placeholder Image';?>">
-                            <p><?=$product->name;?></p>
+                            <p><b><?=htmlspecialchars(strip_tags($product->name), ENT_QUOTES, 'UTF-8');?></b></p>
+                            <p>£<?=$product->price;?></p>
                         </a>
                     </div>
                 <?php endforeach; ?>
@@ -57,15 +58,21 @@
                         else
                             $requestUri = $_SERVER['REQUEST_URI'];
                         
-                        $previousPageNumber = $_GET['page']-1;
-                        $nextPageNumber = $_GET['page']+1;
+                        if (isset($_GET['page'])) {
+                            $previousPageNumber = $_GET['page']-1;
+                            $nextPageNumber = $_GET['page']+1;
+                        }    
+                        else {
+                            $previousPageNumber = 0;
+                            $nextPageNumber = 2;
+                        }
                     ?>
                     
                     <?php if ($previousPageNumber != 0): ?>
                         <?php if (stripos($requestUri, '?') !== false || stripos($requestUri, '&') !== false): ?> 
-                            <li><a href="<?=$requestUri . '&page=' . $previousPageNumber;?>">Previous Page</a></li>
+                            <li><a href="<?=$requestUri . '&page=' . $previousPageNumber;?>"><i class="fas fa-arrow-left"></i></a></li>
                         <?php else: ?>
-                            <li><a href="<?=$requestUri . '?page=' . $previousPageNumber;?>">Previous Page</a></li>
+                            <li><a href="<?=$requestUri . '?page=' . $previousPageNumber;?>"><i class="fas fa-arrow-left"></i></a></li>
                         <?php endif; ?>
                     <?php endif; ?>
 
@@ -79,9 +86,9 @@
                     
                     <?php if ($nextPageNumber <= $totalPages): ?>
                         <?php if (stripos($requestUri, '?') !== false || stripos($requestUri, '&') !== false): ?> 
-                            <li><a href="<?=$requestUri . '&page=' . $nextPageNumber;?>">Next Page</a></li>
+                            <li><a href="<?=$requestUri . '&page=' . $nextPageNumber;?>"><i class="fas fa-arrow-right"></i></a></li>
                         <?php else: ?>
-                            <li><a href="<?=$requestUri . '?page=' . $nextPageNumber;?>">Next Page</a></li>
+                            <li><a href="<?=$requestUri . '?page=' . $nextPageNumber;?>"><i class="fas fa-arrow-right"></i></a></li>
                         <?php endif; ?>
                     <?php endif; ?>
                 </ul>
