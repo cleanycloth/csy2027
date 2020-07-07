e8d77b78638277d56d539d6e00704fc01815cff3
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-07 01:33:51 +0100

    Updated main navigation bar to display categories without any children.

--- templates/nav.html.php
+++ templates/nav.html.php
@@ -1,15 +1,17 @@
 <nav class="site-nav">
     <ul>
-        <?php foreach($categories as $category): ?>
-            <?php if (empty($category->parent_id)):?>
+        <?php foreach ($categories as $category): ?>
+            <?php if (count($category->getChildCategories()) > 0): ?>
                 <li class="dropdown">
-                <a href="/category?name=<?=urlencode($category->name);?>"><?=$category->name;?></a>
-                <ul>
-                    <?php foreach($category->getChildCategories() as $childCategory): ?>
-                        <li><a href="/category?name=<?=urlencode($childCategory->name);?>"><?=$childCategory->name;?></a></li>
-                    <?php endforeach; ?>
-                </ul>
-            </li>
+                    <a href="/products?category=<?=urlencode($category->name);?>"><?=$category->name;?></a>
+                    <ul>
+                        <?php foreach($category->getChildCategories() as $childCategory): ?>
+                            <li><a href="/products?category=<?=urlencode($childCategory->name);?>"><?=$childCategory->name;?></a></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            <?php elseif ($category->parent_id == null && count($category->getChildCategories()) == 0): ?>
+                <li><a href="/products?category=<?=urlencode($category->name);?>"><?=$category->name;?></a></li>
             <?php endif;?>
         <?php endforeach; ?>
     </ul>
