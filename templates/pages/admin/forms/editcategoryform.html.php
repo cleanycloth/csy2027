579136190202755f1df96608e27d1a8c972be465
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-07 01:34:33 +0100

    Updated editcategoryform.html.php to display categories without any children in the category dropdown.

--- templates/pages/admin/forms/editcategoryform.html.php
+++ templates/pages/admin/forms/editcategoryform.html.php
@@ -1,14 +1,12 @@
 <form id="edit-category-form" action="" method="post">
     <output><?=(isset($error)) ? $error : '';?></output>
-    <?php if (!isset($_GET['id'])): ?>
-        <p class="required">Required:</p>
-    <?php endif; ?>
+    <p class="required">Required:</p>
     <div class="fields">    
         <?php if (isset($_GET['id'])): ?>
             <input type="hidden" name="category[category_id]" value="<?=$category->category_id;?>">
         <?php endif; ?>
         
-        <label for="name" <?=(isset($_GET['id'])) ? '' : 'class="required"';?>>Category Name</label>
+        <label for="name" class="required">Category Name</label>
         <input type="text" name="category[name]" id="name" value="<?php if (isset($_GET['id'])) { echo htmlspecialchars(strip_tags($category->name), ENT_QUOTES, 'UTF-8'); } elseif (isset($_POST['category'])) { echo $_POST['category']['name']; } ?>">
     
         <label for="parent-category">Parent Category</label>
@@ -20,10 +18,8 @@
             <option <?=(!isset($parentCategory) || isset($parentCategory) && empty($parentCategory)) ? 'selected="selected"' : '';?> value="">None</option>
 
             <?php foreach ($categories as $categoryChoice): ?>
-                <?php if ($categoryChoice->parent_id == null): ?>
-                    <?php if ($categoryChoice->category_id != $parentCategory->category_id && $categoryChoice->category_id != $category->category_id): ?>
-                        <option value="<?=$categoryChoice->category_id;?>"><?=htmlspecialchars(strip_tags($categoryChoice->name), ENT_QUOTES, 'UTF-8');?></option>
-                    <?php endif; ?>
+                <?php if ($categoryChoice->parent_id == null && count($categoryChoice->getChildCategories()) == 0 || $categoryChoice->parent_id != null): ?>
+                    <option value="<?=$categoryChoice->category_id;?>"><?=htmlspecialchars(strip_tags($categoryChoice->name), ENT_QUOTES, 'UTF-8');?></option>
                 <?php endif; ?>
             <?php endforeach; ?>
         </select>
