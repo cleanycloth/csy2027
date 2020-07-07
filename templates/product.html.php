55f37a4ad81068cfe04c240880098439b66d7256
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-07 23:38:37 +0100

    Updated styling on product page

--- templates/product.html.php
+++ templates/product.html.php
@@ -13,16 +13,16 @@
 
     <div class="pcolumn">
         <h1>Description</h1>
-        <h4>Platform: Platform</h4>
-        <h4>Genre: Genre</h4>
+        <h4>Platform: <?=htmlspecialchars(strip_tags($platformName), ENT_QUOTES, 'UTF-8');?></h4>
+        <h4>Genre: <?=htmlspecialchars(strip_tags($genreName), ENT_QUOTES, 'UTF-8');?></h4>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p><?=htmlspecialchars(strip_tags($product->description), ENT_QUOTES, 'UTF-8');?></p>
     </div>
 
     <div class="pcolumn">
         <div class="purchasebox">
             <h2>Price</h2>
-            <p>£50.00</p>
+            <p>£<?=$product->price;?></p>
             <a href="#">Add to basket</a>
         </div>
     </div>
