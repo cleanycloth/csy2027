5527eb9e2d5c5a88a3f3f055106f2d74c9f5a669
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-09 19:29:03 +0100

    Created BasketControllerTest.php

---
+++ tests/controllers/BasketControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+require 'NNGames/Controllers/BasketController.php';
+class BasketControllerTest extends \PHPUnit\Framework\TestCase {
+    private $basketController;
+
+    public function setUp() {
+        $this->basketController = new \NNGames\Controllers\BasketController(); 
+    }
+
+    /* BasketController Tests */
+    // Home Page Test
+
+}
+?>
