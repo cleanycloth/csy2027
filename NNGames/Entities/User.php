72a4bb9f6251f259771cdeb85771a3b0877ba4dc
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-12 00:48:56 +0100

    Added User entity

---
+++ NNGames/Entities/User.php
@@ -0,0 +1,13 @@
+<?php
+namespace NNGames\Entities;
+class User {
+    public $user_id;
+    public $username;
+    public $firstname;
+    public $surname;
+    public $email;
+    public $password;
+    public $user_type;
+    public $activated;
+}
+?>
