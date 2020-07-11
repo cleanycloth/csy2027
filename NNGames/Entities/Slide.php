323ce323f656c4e6c62942d22802d21435ef1916
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-11 19:01:22 +0100

    Created Slide entity

---
+++ NNGames/Entities/Slide.php
@@ -0,0 +1,10 @@
+<?php
+namespace NNGames\Entities;
+class Slide {
+    public $slide_id;
+    public $image;
+    public $name;
+    public $message;
+    public $url;
+}
+?>
--- NNGames/Routes.php
+++ NNGames/Routes.php
@@ -17,7 +17,7 @@ class Routes implements \CSY2028\Routes {
         $this->usersTable = new \CSY2028\DatabaseTable($pdo, 'users', 'user_id');
         $addressesTable = new \CSY2028\DatabaseTable($pdo, 'addresses', 'address_id');
         $this->productsTable = new \CSY2028\DatabaseTable($pdo, 'products', 'product_id', '\NNGames\Entities\Product', [$this->categoriesTable, $platformsTable, $genresTable]);
-        $this->slidesTable = new \CSY2028\DatabaseTable($pdo, 'slides', 'slide_id');
+        $this->slidesTable = new \CSY2028\DatabaseTable($pdo, 'slides', 'slide_id', '\NNGames\Entities\Slide');
 
         // Redeclare $categoriesTable with new value;
         $this->categoriesTable = new \CSY2028\DatabaseTable($pdo, 'categories', 'category_id', '\NNGames\Entities\Category', [$this->categoriesTable, $this->productsTable]);
