d92674a3f45fa8adeaa7b12f5b5ec8f011b0d65a
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-07-17 12:51:33 +0100

    Applied 'admin-nav' CSS class to accountnav.html.php.

--- templates/accountnav.html.php
+++ templates/accountnav.html.php
@@ -1,9 +1,8 @@
-<nav>
+<nav class="admin-nav">
     <ul>
         <li><a href="/myaccount/addresses">Addresses</a></li>
         <li><a href="/myaccount/payinfo">Payment Info</a></li>
         <li><a href="/myaccount/orders">Order History</a></li>
         <li><a href="/myaccount/settings">Account Settings</a></li>
     </ul>
-    
 </nav>
